1e115ac8144bfab9f20bf2e7b1cf933201a76263
Alex <90839295+Hanterorg@users.noreply.github.com>
2023-12-17 13:15:43 +0300

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -3,6 +3,7 @@
 import discord
 from discord.ext import commands
 import random
+import os
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -13,7 +14,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+bot = commands.Bot(command_prefix='.', description=description, intents=intents)
 
 
 @bot.event
@@ -22,58 +23,27 @@ async def on_ready():
     print('------')
 
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-
-
 @bot.group()
 async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
     if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+        rand = random.randint(1,2)
+        if rand == 1:
+            await ctx.send(f'Нет, {ctx.subcommand_passed} это не круто')
+        elif rand == 2:
+            await ctx.send(f'Да, {ctx.subcommand_passed} это круто')
 
 
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
+@bot.command()
+async def meme(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+
+    await ctx.send(file=picture)
+
+@bot.command()
+async def ask(ctx):
+    
 
 
-bot.run('MTE3ODI2OTgxMjkwMDc3ODAxNA.GRh4X2.rjQBA_S2bN4mTKf94CqBednXus9WEfxWkwe1tg')
+bot.run('token')
